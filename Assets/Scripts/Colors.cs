8f6b1f9e5a0129ca9ed9b69be2fe6a10fcedde86
GregoriyPavlov <GrishaPavlovGame@users.noreply.github.com>
2021-04-14 00:55:08 +0300

    temp

---
+++ Assets/Scripts/Colors.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Colors : MonoBehaviour
+{
+    public static nColor[] ListOfColors;
+}
+
+[System.Serializable]
+public struct nColor
+{
+    public string Name;
+    public Color color;
+
+}
+
